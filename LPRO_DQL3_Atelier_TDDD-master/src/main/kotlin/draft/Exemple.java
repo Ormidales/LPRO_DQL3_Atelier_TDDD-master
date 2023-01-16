f39001f563aa1b861f7f5eb66a74bb342a80675f
Hugo Doueil <46538211+Ormidales@users.noreply.github.com>
2023-01-16 15:07:10 +0100

    Add files via upload

---
+++ LPRO_DQL3_Atelier_TDDD-master/src/main/kotlin/draft/Exemple.java
@@ -0,0 +1,17 @@
+package main.kotlin.draft;
+
+public class Exemple implements IExemple {
+    private int Price;
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public void augmenter(int i)  {
+        Price += i;
+    }
+}
